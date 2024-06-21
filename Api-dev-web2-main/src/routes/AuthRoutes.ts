3e43c4370b5a7f68b0193387bfb5dae9ae24a5bc
VINÍCIUS DIAS DE OLIVEIRA <97258401+Suiciniv96@users.noreply.github.com>
2024-06-21 20:50:27 -0300

    Delete Api-dev-web2-main directory

--- Api-dev-web2-main/src/routes/AuthRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthController from "../controllers/AuthController";
-
-const AuthRouter = Router();
-
-AuthRouter.post("/api/auth/signin", AuthController.signIn);
-
-
-export default AuthRouter;
